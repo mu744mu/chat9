0387acacea33c5d1adc28adfa82da42976d5546f
https://ita7a.com <80185284+mu744mu@users.noreply.github.com>
2022-04-22 00:51:12 +0300

    Create jawal.js

---
+++ jawal.js
@@ -0,0 +1,21 @@
+$('.hand.nosel.fl.uzr.border').css({'margin': '2px 2',borderRight: '5px solid #FFFFFF',borderLeft: '5px solid #F7F7F7',borderTop: '3px solid #F7F7F7',borderBottom: '2px solid #FFFFFF',paddingTop: '5px',borderRadius: '7px'});
+$(`<link rel="stylesheet" href="">
+<style> .fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.080), 0 0 10px rgb(118, 118, 22226);border-radius: 90px 90px 90px 90px;} <div class="lonline light break" style="width:100%;outline: lightgray solid 1px;">
+<style>
+.fa-comment:before {margin-left: 12px; }.fa-group:before, .fa-users:before { margin-left: 13px; }.fa-commenting-o:before {margin-left: 14px; }.fa-gear:before, .fa-cog:before { margin-left: 15px; }
+span.minix.badge.border,span.busers.minix.badge.border {
+    margin-left: 2px;
+    background-color: #a94442;
+    border: #d9534f solid 1px!important;
+}
+label.ae.fa.label.label-primary.fa-user {
+    width: 50px;
+}
+
+div#tlogins .hand.nosel.fl.uzr.border{background:#;border:1px solid #!important;border-radius:6px!important;padding:4px!important}div#tlogins .fl.mini.u-msg{font-size:90%!important;padding:4px!important;font-family:'jazeera',FontAwesome;margin:19px 16px 18px 15px}div#tlogins img.fitimg.fl.u-pic{background-size:cover;min-width:48px;min-height:48px;max-height:62px;margin-top:1px;border-radius:50px!important;box-shadow:0 2px 2px 0 rgba(0,0,0,0.14),0 1px 5px 0 rgba(0,0,0,0.12),0 3px 1px -2px rgba(0,0,0,0.2)!important}
+</style>`).insertBefore("body");
+
+$('.ae.label.label-primary.fa.fa-gear').html(`⠀`);
+$('label.ae.fa.label.label-primary.fa-commenting-o').html(`⠀`);
+$('.ae.fa.label.label-primary.fa-users').html(`⠀`);
+$(".hand.nosel.fl.uzr.border").css({margin:"1px 0",borderRadius:"0px"}),$(".checkbox label").html(`<input id="stealth"type="checkbox"value=""><i class="fa fa-eye-slash"aria-hidden="true"style="margin-top: 3px"></i>`),
